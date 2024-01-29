72ff95dfc34e0eee7a6a6ca44f667a451349b728
kurs <kurs@DESKTOP-V3HDKPQ>
2024-01-29 10:03:06 +0100

    q

--- P08ShopWebApp.Client/Pages/Calculator.cshtml.cs
+++ P08ShopWebApp.Client/Pages/Calculator.cshtml.cs
@@ -5,6 +5,11 @@ namespace P08ShopWebApp.Client.Pages
 {
     public class CalculatorModel : PageModel
     {
+        public int Result { get; set; }
+
+        public int Number1 { get; set; }
+        public int Number2 { get; set; }
+
         public void OnGet()
         {
            
@@ -16,6 +21,19 @@ namespace P08ShopWebApp.Client.Pages
 
 			bool r1 = int.TryParse(form["Number1"], out int number1);
 			bool r2 = int.TryParse(form["Number2"], out int number2);
+
+            // ulepszamy rozwiazanie o przesyanie na widok takze wartosci poczatkowych 
+            Number1 = number1;
+            Number2 = number2;
+
+            if (r1 && r2) 
+            {
+                Result = number1 + number2;
+            }
+            else
+            {
+                // obsuga bledu 
+            }
 		}
     }
 }
